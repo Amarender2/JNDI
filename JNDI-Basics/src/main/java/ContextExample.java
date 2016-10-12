cd7d9416d4adc3133d0a5df82f13d826d0e649f4
armekala <ammv8@mst.edu>
2016-10-12 17:19:32 -0500

    Name Interface, Composite and Compound Name

--- JNDI-Basics/src/main/java/ContextExample.java
+++ JNDI-Basics/src/main/java/ContextExample.java
@@ -3,11 +3,17 @@ package main.java;
 import java.io.File;
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Properties;
 
 import javax.naming.Binding;
+import javax.naming.CompositeName;
+import javax.naming.CompoundName;
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.InvalidNameException;
+import javax.naming.Name;
 import javax.naming.NameClassPair;
+import javax.naming.NameParser;
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 
@@ -33,27 +39,86 @@ public class ContextExample {
 
 		// bind(context);
 
+		// lookup(context);
+
+		// createDestroySubContext(context);
+
+		// printList(context);
+		
+		// printListBindings(context);
+		
+		
+		// compositeName();
+		
+		// compundName(context);
+		
+		nameSpace(context);
+		
+	}
+	
+	private static void nameSpace(Context context) throws NamingException {
+		Context otherContext =  (Context) context.lookup("sample");
+		System.out.println(otherContext.getNameInNamespace());
+		
+		String name = otherContext.composeName("sample", "Users");
+		System.out.println(name);
+	}
+	
+	private static Name compundName(Context context) throws NamingException {
+		Properties properties = new Properties();
+		properties.put("jndi.syntax.direction", "right_to_left");
+		properties.put("jndi.syntax.separator", ",");
+		properties.put("jndi.syntax.escape", ":");
+		CompoundName compoundName = new CompoundName("some,first::second,aaa:,bbb:,ccc", properties);
+		Enumeration<String> components = compoundName.getAll();
+		while(components.hasMoreElements()) {
+			System.out.println(components.nextElement());
+		}
+		
+		// NameParser to parse string to Name, returns compoundName
+		NameParser nameParser = context.getNameParser("");
+		Name name = nameParser.parse("first/second/third");
+		name.add(0, "headreplace");
+		name.remove(1);
+		System.out.println(name);
+		
+		components = name.getAll();
+		while(components.hasMoreElements()) {
+			System.out.println(components.nextElement());
+		}
+		
+		return compoundName;
+		
+	}
+	
+	private static Name compositeName() throws InvalidNameException {
+		CompositeName compositeName = new CompositeName();
+		compositeName.add("first.name");
+		compositeName.add("sample");
+		System.out.println(compositeName);
+		return compositeName;
+		// first.name/sample\name
+	}
+	
+	private static void createDestroySubContext(Context context) throws NamingException {
+		context.createSubcontext("first.name");
+		context.createSubcontext("sample");
+		//context.destroySubcontext("sample/folder1");
+	}
+	
+	private static void lookup(Context context) throws NamingException {
 		Object employee = context.lookup("amar");
 		// With and Without object Factory It works. One gives Reference, Other
 		// Employee Object
 		System.out.println("Class : " + employee.getClass().getName());
 		System.out.println(employee);
-
-		// context.createSubcontext("sample/folder1");
-		// context.createSubcontext("sample/folder2");
-		// context.destroySubcontext("sample/folder1");
-
-		printList(context);
-		
-		printListBindings(context);
-
 	}
 
 	private static void printListBindings(Context context) throws NamingException {
 		NamingEnumeration<Binding> bindings = context.listBindings("");
 		while(bindings.hasMore()) {
 			Binding binding = bindings.next();
-			System.out.println(binding.getName() + " , "+ binding.getClassName());
+			System.out.print(binding.getName() + "\t "+ binding.getClassName() + "\t");
 			if(binding.getObject() instanceof File) {
 				System.out.println(((File)binding.getObject()).length());
 			}
@@ -65,7 +130,7 @@ public class ContextExample {
 
 		while (contextData.hasMoreElements()) {
 			NameClassPair nameClassPair = contextData.nextElement();
-			System.out.println(nameClassPair.getName() + " , " + nameClassPair.getClassName());
+			System.out.println(nameClassPair.getName() + "\t" + nameClassPair.getClassName());
 		}
 	}
 	
